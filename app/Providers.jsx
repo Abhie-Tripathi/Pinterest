2233921251bad1a73c564196cae6e223a29d3e64
Abhie-Tripathi <abhiiii.tripathi@gmail.com>
2023-07-13 10:59:18 +0530

    Provider fix

--- app/Providers.jsx
+++ app/Providers.jsx
@@ -1,7 +1,7 @@
 "use client"
 import { SessionProvider } from "next-auth/react"
 
-const Providers = ({children}) => {
+const Providers = () => {
   return (
     <SessionProvider>
       {children}
